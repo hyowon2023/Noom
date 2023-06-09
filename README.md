06761e45ed8d93394b80fdb16ac62e653127c9cd
hyowon2023 <hwshin2023@gmail.com>
2023-06-10 01:19:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Noom
+
+Zoom Clone using NodeJS, WebRTC and Websokets.
+
+## 기능
+
+- 실시간 채팅 기능 구현하기
+- 채팅룸 기능 구현하기
+- 화상 채팅 기능 구현하기
---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import SocketIO from 'socket.io';
+import http from 'http';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
+
+const server = http.createServer(app);
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on('connection', (socket) => {
+  socket.on('enter_room', (roomName, done) => {
+    done();
+    socket.join(roomName);
+    socket.to(roomName).emit('welcome');
+  });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => socket.to(room).emit('bye'));
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', msg);
+    done();
+  });
+});
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+httpServer.listen(3000, handleListen);
